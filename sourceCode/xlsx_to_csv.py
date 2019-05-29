593387d9d2d64dea36cb9fe1f3a32c6e61cd61d8
KylinC <k1017856853@icloud.com>
2019-05-29 15:55:44 +0800

    standard form of .csv for all the data

--- sourceCode/xlsx_to_csv.py
+++ sourceCode/xlsx_to_csv.py
@@ -6,6 +6,6 @@ def xlsx_to_csv_pd(path,fileName):
     data_xls.to_csv('../csvData/'+fileName[:-4]+"csv", encoding='utf-8')
 
 if __name__ == '__main__':
-	path=input("RelativePath: ")
-	fileName=input("FileName: ")
+    path=input("RelativePath: ")
+    fileName=input("FileName: ")
     xlsx_to_csv_pd(path,fileName)
